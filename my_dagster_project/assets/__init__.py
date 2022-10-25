2ba10a63bc92fcb12da06e645348edf99a2eeb03
Alvaro Clemente <clemente@bigml.com>
2022-10-25 20:32:37 +0200

    feat: gist working

--- my_dagster_project/assets/__init__.py
+++ my_dagster_project/assets/__init__.py
@@ -1,11 +1,15 @@
 from __future__ import annotations
 
 import os
+import pickle
 from datetime import timedelta
 
+import jupytext  # type: ignore
+import nbformat
 import pandas as pd
 from dagster import asset
-from github import Github
+from github import Github, InputFileContent
+from nbconvert.preprocessors import ExecutePreprocessor  # type: ignore
 
 ACCESS_TOKEN = os.environ["GITHUB_TOKEN"]
 
@@ -13,7 +17,9 @@ ACCESS_TOKEN = os.environ["GITHUB_TOKEN"]
 @asset
 def github_stargazers():
     return list(
-        Github(ACCESS_TOKEN).get_repo("dagster-io/dagster").get_stargazers_with_dates()
+        Github(ACCESS_TOKEN)
+        .get_repo("dagster-io/dagster")
+        .get_stargazers_with_dates()
     )
 
 
@@ -30,3 +36,41 @@ def github_stargazers_by_week(github_stargazers):
         ]
     )
     return df.groupby("week").count().sort_values(by="week")
+
+
+@asset
+def github_stars_notebook(github_stargazers_by_week):
+    markdown = f"""
+# Github Stars
+
+```python
+import pickle
+
+github_stargazers_by_week = pickle.loads({pickle.dumps(github_stargazers_by_week)})
+```
+
+## Github Stars by Week, last 52 weeks
+
+```python
+github_stargazers_by_week.tail(52).reset_index().plot.bar(x="week", y="users")
+```
+    """
+    nb = jupytext.reads(markdown, "md")
+    ExecutePreprocessor().preprocess(nb)
+    return nbformat.writes(nb)
+
+
+@asset
+def github_stars_noteboook_gist(context, github_stars_notebook):
+    gist = (
+        Github(ACCESS_TOKEN)
+        .get_user()
+        .create_gist(
+            public=False,
+            files={
+                "github_stars.ipynb": InputFileContent(github_stars_notebook)
+            },
+        )
+    )
+    context.log.info("Notebook created at %s", gist.html_url)
+    return gist.html_url
--- my_dagster_project/repository.py
+++ my_dagster_project/repository.py
@@ -1,8 +1,19 @@
-from dagster import load_assets_from_package_module, repository
+from dagster import (
+    ScheduleDefinition,
+    define_asset_job,
+    load_assets_from_package_module,
+    repository,
+)
 
 from my_dagster_project import assets
+
+daily_job = define_asset_job(name="daily_refresh", selection="*")
+daily_schedule = ScheduleDefinition(
+    job=daily_job,
+    cron_schedule="@daily",
+)
 
 
 @repository
 def my_dagster_project():
-    return [load_assets_from_package_module(assets)]
+    return [daily_job, daily_schedule, load_assets_from_package_module(assets)]
